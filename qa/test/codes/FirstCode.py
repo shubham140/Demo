fb7eadc879af3e1cc40d2652aad04667bed28c9c
Shubham <shubhamchawla140@gmail.com>
2021-05-30 20:37:24 +0530

    MyfirstCommit

---
+++ qa/test/codes/FirstCode.py
@@ -0,0 +1,14 @@
+
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:\\Users\\Shubham\\OneDrive\\Desktop\\Study\\Selenium\\Broswer_Files\\chromedriver.exe")
+# Navigate to url
+
+driver.get("https://www.udemy.com/")
+
+
+
+#
+
+
+
